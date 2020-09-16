1a8395b490b79b542a7cdfe80f9af29af2a63811
gameclub000 <2838983406@qq.com>
2020-09-16 19:51:32 +0800

    test(runner): set unit test flow

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import { Provider } from 'react-redux';
-import store from '@/store';
+import store from '../store';
 
 function MyApp({ Component, pageProps }) {
     return (
